8429dcfeca22f11539f02c0caa036f5793fddf2b
anastasiia.mishchuk <anastasiya.mishchuk@gmail.com>
2020-01-13 23:27:47 +0100

    Fixed solver lr

--- delineation/solver/solver.py
+++ delineation/solver/solver.py
@@ -6,6 +6,6 @@ def make_optimizer(cfg, model):
     if cfg.TRAINING.OPTIMIZER == 'sgd':
         optimizer = getattr(torch.optim, cfg.TRAINING.OPTIMIZER)(model.parameters(), momentum=cfg.TRAINING.MOMENTUM)
     else:
-        optimizer = getattr(torch.optim, cfg.TRAINING.OPTIMIZER)(model.parameters())
+        optimizer = getattr(torch.optim, cfg.TRAINING.OPTIMIZER)(model.parameters(), lr=cfg.TRAINING.BASE_LR)
 
     return optimizer
